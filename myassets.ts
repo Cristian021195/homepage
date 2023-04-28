6e8e724fdc0a51df4d161821ce09d78886d865f7
unknown <cristian021195>
2023-04-28 11:30:53 -0300

    segundo commit, prueba de mas secciones

--- myassets.ts
+++ myassets.ts
@@ -1,11 +1,32 @@
-import horabondi1 from './public/images/projects/horabondi/empresas.webp'
-import horabondi2 from './public/images/projects/horabondi/contacto.webp'
-import horabondi3 from './public/images/projects/horabondi/main.webp'
+import hb1 from './public/images/projects/horabondi/empresas.webp'
+import hb2 from './public/images/projects/horabondi/contacto.webp'
+import hb3 from './public/images/projects/horabondi/main.webp'
+import hb4 from './public/images/projects/horabondi/Screenshot_20221221-153325.png'
+import hb5 from './public/images/projects/horabondi/Screenshot_20221221-153332.png'
+import hb6 from './public/images/projects/horabondi/Screenshot_20221221-153338.png'
+import hb7 from './public/images/projects/horabondi/Screenshot_20221221-153410.png'
+import hb8 from './public/images/projects/horabondi/Screenshot_20221221-153433.png'
+
+import rm1 from "./public/images/projects/romano/main.webp"
+import rm2 from "./public/images/projects/romano/about.webp"    
+import rm3 from "./public/images/projects/romano/eventos.webp"
+import rm4 from "./public/images/projects/romano/menu.webp"
+import rm5 from "./public/images/projects/romano/Screenshot_20230331-081103.png"
+import rm6 from "./public/images/projects/romano/Screenshot_20230331-081145.png"
+import rm7 from "./public/images/projects/romano/Screenshot_20230331-081215.png"
+import rm8 from "./public/images/projects/romano/Screenshot_20230331-081233.png"
+
+import hbb1 from './public/images/projects/horabondi-beta/Screenshot_20221221-152836.png'
+import hbb2 from './public/images/projects/horabondi-beta/Screenshot_20221221-152843.png'
+import hbb3 from './public/images/projects/horabondi-beta/Screenshot_20221221-152849.png'
+import hbb4 from './public/images/projects/horabondi-beta/Screenshot_20221221-152913.png'
+import hbb5 from './public/images/projects/horabondi-beta/Screenshot_20221221-152926.png'
+
 import pmain from './public/portfolio-main.svg'
 
-export const horabondi = [
-    horabondi1,horabondi2, horabondi3
-];
+export const horabondi = [ hb1,hb2,hb3,hb4,hb5,hb6,hb7,hb8 ];
+export const romano = [ rm1,rm2,rm3,rm4,rm5,rm6,rm7,rm8 ];
+export const horabondi_beta = [ hbb1,hbb2,hbb3,hbb4,hbb5 ];
 
 export const portfolio_main = {
     pmain
---
+++ pages/portfolio/horabondi-beta.tsx
@@ -0,0 +1,75 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+//import styles from '@/styles/Home.module.css'
+
+import localFont from 'next/font/local'
+import { Header, MainDesc } from '@/components/layout'
+import { MainLayout, NoHome } from '../../layouts';
+import { SquareImg } from '@/components/other';
+
+import {GalleryPortfolio} from '../../components/ui/';
+import { useState } from 'react';
+
+import {horabondi_beta} from '../../myassets'
+
+export default function HorabondiBeta() { // yarn remove react-grid-gallery
+  const [open, setOpen] = useState(false);
+  return (
+    <MainLayout title='HorabondiBeta'>
+      <NoHome>
+          <div className='col-md-6 d-flex fade-up flex-wrap'>
+            <MainDesc title='Horabondi beta'>
+              <div>
+              <p className=''>
+              It's beta version of HoraBondi, made entirely in PHP and Vanilla JS. a PWA focused in offline usage. Handling admin users, bus schedule and pricing travels. <br />
+              This application was made for testing purposes.<br /><br />
+              </p>
+              <br />
+              <i><b>Application delivered time: </b>4 months</i>
+              </div>
+              <div className='mt-5'>
+                <a className="btn p-2 bg-pink-0" target='_blank' href='https://cristian021195.github.io/notas/'>¡Try It!</a>
+                <a className="btn p-2 bg-lblue-0 m-2" target='_blank' href='https://github.com/Cristian021195/horabondi-beta'>Go to repository</a>
+              </div>
+            </MainDesc>
+          </div>
+          <div className='col-md-6 col-sm-12 d-flex fade-up row d-flex justify-content-center'>
+            <section className='mt-10 row col-sm-12'>
+              <div className="col-sm-12">
+                <h2 className='fs-2 mb-4'>Resources</h2>
+                <h3>Gallery</h3>
+              </div>
+              
+              <div className="d-flex overflow-x h-scroll p-2 overflow-y gap-1 my-4 view-gallery br-1">
+                <SquareImg alt='Photo' src={horabondi_beta[0]}  />
+                <SquareImg alt='Photo' src={horabondi_beta[1]}  />
+                <SquareImg alt='Photo' src={horabondi_beta[2]}  />
+                <button className='btn bg-pink-0 p-1' style={{position:'absolute'}} onClick={()=>setOpen(true)}>Ver Fotos</button>
+              </div>
+              <div>
+              <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                slides={horabondi_beta}
+                render={{ slide: GalleryPortfolio }}
+              />
+              </div>
+
+              <h3>Technologies and other resources</h3>
+              <div className="d-flex flex-wrap p-4 gap-6 mb-4">
+                <ul>
+                  <li><b>Programing Languajes: </b>PHP, JavaScript, HTML + CSS</li>
+                  <li><b>Database Engine: </b>MySQL</li>
+                  <li><b>Deploy Platform: </b>000Webhost</li>
+                  <li><b>Additional Packages: </b>PageJS, Materialize, One Signal API</li>
+                </ul>
+              </div>
+            </section>            
+          </div>
+      </NoHome>
+    </MainLayout>
+  )
+}
