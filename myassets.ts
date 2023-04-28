20e34c4da0fe8a843921ed506f3ae255074681f6
unknown <cristian021195>
2023-04-28 11:39:05 -0300

    tercer commit ordenado myassets

--- myassets.ts
+++ myassets.ts
@@ -22,11 +22,30 @@ import hbb3 from './public/images/projects/horabondi-beta/Screenshot_20221221-15
 import hbb4 from './public/images/projects/horabondi-beta/Screenshot_20221221-152913.png'
 import hbb5 from './public/images/projects/horabondi-beta/Screenshot_20221221-152926.png'
 
+import cl1 from './public/images/projects/colores/Screenshot_20230330-162026.png'
+import cl2 from './public/images/projects/colores/Screenshot_20230330-162031.png'
+import cl3 from './public/images/projects/colores/Screenshot_20230330-163708.png'
+
+import cm1 from './public/images/projects/climap/Screenshot_20221221-153243.png'
+import cm2 from './public/images/projects/climap/Screenshot_20221221-153257.png'
+import cm3 from './public/images/projects/climap/Screenshot_20221221-153307.png'
+import cm4 from './public/images/projects/climap/main.webp'
+
+import es1 from './public/images/projects/escritos/Screenshot_20221221-152505.png'
+import es2 from './public/images/projects/escritos/Screenshot_20221221-152538.png'
+import es3 from './public/images/projects/escritos/main.webp'
+import es4 from './public/images/projects/escritos/Screenshot_20230330-162254.png'
+import es5 from './public/images/projects/escritos/Screenshot_20230330-162343.png'
+import es6 from './public/images/projects/escritos/Screenshot_20230330-162352.png'
+
 import pmain from './public/portfolio-main.svg'
 
 export const horabondi = [ hb1,hb2,hb3,hb4,hb5,hb6,hb7,hb8 ];
 export const romano = [ rm1,rm2,rm3,rm4,rm5,rm6,rm7,rm8 ];
 export const horabondi_beta = [ hbb1,hbb2,hbb3,hbb4,hbb5 ];
+export const colores = [ cl1,cl2,cl3 ];
+export const climap = [ cm1,cm2,cm3,cm4 ];
+export const escritos = [ es1,es2,es3,es4,es5,es6 ];
 
 export const portfolio_main = {
     pmain
