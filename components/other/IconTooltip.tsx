4f5e20bcb69c90f449a4da7f0e53b5f2ed2f9ee5
unknown <cristian021195>
2023-04-28 12:31:20 -0300

    mejoramos IconTooltip

--- components/other/IconTooltip.tsx
+++ components/other/IconTooltip.tsx
@@ -9,18 +9,18 @@ interface IProps{
 
 export const IconTooltip = ({title = "Project", img="https://cristian021195.github.io/portfolio/img/favicon.svg", hover="hithere"}:IProps) => {
   return (
-    <a href={img} target='_blank' className="tt-c" data-position="bottom">
+    <button className="tt-c" data-position="bottom">
         <Image src={img} alt="colores" width={60} className={hover} height={60}/>
-        <span className="toolTip">{title}</span>
-    </a>
+        <span className="toolTip"><a href={img} target='_blank' className='white-0'>{title}</a></span>
+    </button>
   )
 }
 
 /*
 
-<a href='https://cristian021195.github.io/portfolio/img/svg/horabondi.svg' target='_blank' className='tooltip-container'>
-            <Image src='https://cristian021195.github.io/portfolio/img/svg/horabondi.svg' title='Horabondi' alt="colores" width={160} height={160} className='p-4' style={{borderRadius:'1em 1em 0 0', objectFit:'fill'}}/>
-            <span className="tooltip">My Awesome ToolTip</span>
-        </a>
+<a href={img} target='_blank' className="tt-c" data-position="bottom">
+        <Image src={img} alt="colores" width={60} className={hover} height={60}/>
+        <span className="toolTip">{title}</span>
+    </a>
 
 */
